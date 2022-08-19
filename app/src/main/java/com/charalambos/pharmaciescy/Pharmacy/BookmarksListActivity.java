4467050cfb541b321fa223a22ae6a749bc16a8ae
cmaxoutis79 <cmaxoutis79@gmail.com>
2022-08-20 01:24:13 +0300

    Fixed bugs due to renaming

--- app/src/main/java/com/charalambos/pharmaciescy/Pharmacy/BookmarksListActivity.java
+++ app/src/main/java/com/charalambos/pharmaciescy/Pharmacy/BookmarksListActivity.java
@@ -3,17 +3,18 @@ package com.charalambos.pharmaciescy.Pharmacy;
 import androidx.appcompat.app.ActionBar;
 
 import com.charalambos.pharmaciescy.Bookmarks.Bookmarks;
+import com.charalambos.pharmaciescy.Pharmacy.internal.MyFilter;
 import com.charalambos.pharmaciescy.Settings.Settings;
 
-public class BookmarksListActivity extends PharmacyListActivity {
+public class BookmarksListActivity extends AllListActivity {
     @Override
     protected String withTitle() {
         return "Αγαπημένα φαρμακεία";
     }
 
     @Override
-    protected PharmacyFilter buildFilter(Settings settings, Bookmarks bookmarks) {
-        return new PharmacyFilter.PharmacyFilterBuilder()
+    protected MyFilter buildFilter(Settings settings, Bookmarks bookmarks) {
+        return new MyFilter.PharmacyFilterBuilder()
                 .setDistrictsFilter(settings.getSelectedDistrictsPreference())
                 .withBookmarksOnly(bookmarks)
                 .build();
